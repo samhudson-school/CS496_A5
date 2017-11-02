18d58e1019e0159658e78de6da0e98adfd87de05
Sam Hudson <Sam_Hudson@symantec.com>
2017-11-01 20:47:05 -0700

    Main Activity links to Data Entry Page & Style modifications

--- app/src/main/java/com/example/samuelhudson/assignment5/MainActivity.java
+++ app/src/main/java/com/example/samuelhudson/assignment5/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.samuelhudson.assignment5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t1,t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = (TextView) findViewById(R.id.DataEntry);
+        t2 = (TextView) findViewById(R.id.DataView);
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DataEntry.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
